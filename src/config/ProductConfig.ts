92954ca179913c4d70c57a8bf4bbafc14016d5d1
Zala145 <jakub.zalecki@gmail.com>
2024-06-03 11:22:18 +0200

    Update ProductConfig.ts

--- src/config/ProductConfig.ts
+++ src/config/ProductConfig.ts
@@ -1,11 +1,25 @@
-export const ProductConfig = {
+type ConfigType = 'string' | 'choice' | 'number' | 'bool';
+
+interface ConfigField {
+  type: ConfigType;
+  label: string;
+}
+
+interface ProductConfig {
+  name: ConfigField;
+  size: ConfigField;
+  price: ConfigField;
+  available: ConfigField;
+}
+
+export const ProductConfig: ProductConfig = {
   name: {
     type: 'string',
     label: 'Name',
   },
   size: {
     type: 'choice',
-    label: 'Size'
+    label: 'Size',
   },
   price: {
     type: 'number',
